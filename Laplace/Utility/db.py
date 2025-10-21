4ec81fee5205ce0622e95b26a53bee550f29b8b9
sodersys <nyanemyko@gmail.com>
2025-10-20 20:40:11 -0500

    doneS!!

--- Laplace/Utility/db.py
+++ Laplace/Utility/db.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import db, credentials
 
 def init():
-    cred = credentials.ApplicationDefault()  # Must call it!
+    cred = credentials.ApplicationDefault()
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://iapetus-74c01-default-rtdb.firebaseio.com/'
     })
