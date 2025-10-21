df71d380342cec4c7f3c7ad36884bfb13d804bb8
sodersys <nyanemyko@gmail.com>
2025-10-20 20:39:55 -0500

    fixin stuff

--- Laplace/Utility/db.py
+++ Laplace/Utility/db.py
@@ -2,10 +2,11 @@ import firebase_admin
 from firebase_admin import db, credentials
 
 def init():
-     firebase_admin.initialize_app(credential=credentials.ApplicationDefault(), options={
-          'databaseURL': 'https://iapetus-74c01-default-rtdb.firebaseio.com/'
-     })
+    cred = credentials.ApplicationDefault()  # Must call it!
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://iapetus-74c01-default-rtdb.firebaseio.com/'
+    })
 
 def get(path: str):
-     ref = db.reference("data/"+path+"/discordId")
-     return str(ref.get())
+    ref = db.reference(f"data/{path}/discordId")
+    return str(ref.get())
