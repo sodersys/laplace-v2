9f45f7560d31586361a0263489947e6a4d7872b2
sodersys <nyanemyko@gmail.com>
2025-10-26 09:51:56 -0500

    fixed command sync

--- Laplace/bot.py
+++ Laplace/bot.py
@@ -1,18 +1,17 @@
 import os
-import hikari, lightbulb
+import hikari
+import lightbulb
 
 bot: hikari.GatewayBot
 
 def init():
-     global bot
-     bot = hikari.GatewayBot(token = os.getenv("botToken"))
-     client = lightbulb.client_from_app(bot)
-     
-     @bot.listen(hikari.StartingEvent)
-     async def on_starting(_: hikari.StartingEvent) -> None:
-          await client.load_extensions("Laplace.Commands.Verification", "Laplace.Commands.UpdateDB")
-          await client.sync_application_commands()
-          await client.start()
+    global bot
+    bot = hikari.GatewayBot(token=os.getenv("botToken"))
+    client = lightbulb.client_from_app(bot)
 
+    @bot.listen(hikari.StartedEvent)
+    async def on_started(_: hikari.StartedEvent) -> None:
+        await client.load_extensions("Laplace.Commands.Verification", "Laplace.Commands.UpdateDB")
+        await client.sync_application_commands(force=True)
 
-     bot.run()
+    bot.run()
