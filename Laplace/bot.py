bf64b5ea7908af3899a695681838f98dbb0f663e
sodersys <nyanemyko@gmail.com>
2025-10-26 09:43:31 -0500

    force command sync

--- Laplace/bot.py
+++ Laplace/bot.py
@@ -12,5 +12,7 @@ def init():
      async def on_starting(_: hikari.StartingEvent) -> None:
           await client.load_extensions("Laplace.Commands.Verification", "Laplace.Commands.UpdateDB")
           await client.start()
+          await client.sync_application_commands()
+
 
      bot.run()
